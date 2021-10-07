d95ce124db1932cccc261740ffe5475511573575
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-10-07 10:45:31 +0200

    Lesson 1 - Step 3: ngStyle directive for background color

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  studentList = [
+    { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M" },
+    { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M" },
+    { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F" },
+    { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M" },
+    { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F" },
+    { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F" },
+    { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M" },
+    { "name": "Martina Velardi", "hobby": "Karate", "gender": "F" },
+    { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M" },
+    { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M" }
+  ]
+
+  student:any;
+
+  constructor() {
+    let num = Math.floor(Math.random() * this.studentList.length);
+    this.student = this.studentList[num];
+  }
 
   ngOnInit(): void {
   }

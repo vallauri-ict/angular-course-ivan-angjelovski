9f43b74ef19ce46c146ebd3121d8b238ac7755b1
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-10-14 10:57:39 +0200

    Lesson 2 -  Step 1: ngIf directive

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -10,19 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class StudentComponent implements OnInit {
 
   studentList = [
-    { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M" },
-    { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M" },
-    { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F" },
-    { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M" },
-    { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F" },
-    { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F" },
-    { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M" },
-    { "name": "Martina Velardi", "hobby": "Karate", "gender": "F" },
-    { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M" },
-    { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M" }
+    { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M", isPro: false },
+    { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M", isPro: true },
+    { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F", isPro: false },
+    { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M", isPro: true },
+    { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F", isPro: false },
+    { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F", isPro: true },
+    { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M", isPro: false },
+    { "name": "Martina Velardi", "hobby": "Karate", "gender": "F", isPro: false },
+    { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M", isPro: true },
+    { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M", isPro: false }
   ]
 
-  student:any;
+  student: any;
 
   constructor() {
     this.randomStudent();

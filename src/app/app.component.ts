2513ca4eb70856fcaa1abbdbf6794876536778f9
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-10-21 11:09:01 +0200

    Lesson 3 - Step 1: Introduction to ndModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("txtName") txtName!:ElementRef;
+  // = new ElementRef(null);
+
   title = '5 INF B 2021/2022';
 
-  studentList = [
-    { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M", isPro: false },
-    { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M", isPro: false },
-    { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F", isPro: false },
-    { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M", isPro: false },
-    { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F", isPro: false },
-    { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F", isPro: false },
-    { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M", isPro: false },
-    { "name": "Martina Velardi", "hobby": "Karate", "gender": "F", isPro: false },
-    { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M", isPro: false },
-    { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M", isPro: false }
-  ]
+  // studentList = [
+  //   { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M", "isPro": false },
+  //   { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M", "isPro": false },
+  //   { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F", "isPro": false },
+  //   { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M", "isPro": false },
+  //   { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F", "isPro": false },
+  //   { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F", "isPro": false },
+  //   { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M", "isPro": false },
+  //   { "name": "Martina Velardi", "hobby": "Karate", "gender": "F", "isPro": false },
+  //   { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M", "isPro": false },
+  //   { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M", "isPro": false }
+  // ]
+
+  studentList:any[] = [];
+
+  studentName:string = "";
+  studentGender:string = "";
+
+  onAddStudent() {
+    let newStudent = {
+      "name": this.studentName, "hobby": "Karate", "gender": this.studentGender, "isPro": false
+    }
+    this.studentList.push(newStudent);
+    this.studentName = "";
+    this.txtName.nativeElement.focus();
+  }
 }

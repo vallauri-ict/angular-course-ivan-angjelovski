408031e582314253aa3aa3deb6577b5bd0370fa5
ivan-angjelovski <62544677+ivan-angjelovski@users.noreply.github.com>
2021-10-21 11:33:49 +0200

    Lesson 3 - Step 2: Little add student form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,45 @@ export class AppComponent {
 
   title = '5 INF B 2021/2022';
 
-  // studentList = [
-  //   { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M", "isPro": false },
-  //   { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M", "isPro": false },
-  //   { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F", "isPro": false },
-  //   { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M", "isPro": false },
-  //   { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F", "isPro": false },
-  //   { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F", "isPro": false },
-  //   { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M", "isPro": false },
-  //   { "name": "Martina Velardi", "hobby": "Karate", "gender": "F", "isPro": false },
-  //   { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M", "isPro": false },
-  //   { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M", "isPro": false }
-  // ]
+  studentRepository = [
+    { "name": "Ettore Esposito", "hobby": "Karate", "gender": "M", "isPro": false },
+    { "name": "Luca Pelissero", "hobby": "Paddle", "gender": "M", "isPro": false },
+    { "name": "Bianca Teleman", "hobby": "Volley", "gender": "F", "isPro": false },
+    { "name": "Paolo Giordano", "hobby": "Volley", "gender": "M", "isPro": false },
+    { "name": "Giada Valinotto", "hobby": "Paddle", "gender": "F", "isPro": false },
+    { "name": "Marta Baudracco", "hobby": "Volley", "gender": "F", "isPro": false },
+    { "name": "Michele Ghisolfi", "hobby": "Paddle", "gender": "M", "isPro": false },
+    { "name": "Martina Velardi", "hobby": "Karate", "gender": "F", "isPro": false },
+    { "name": "Ivan Angjelovski", "hobby": "Paddle", "gender": "M", "isPro": false },
+    { "name": "Gabriele Leone", "hobby": "Volley", "gender": "M", "isPro": false }
+  ]
+
+  hobbies = [
+    "Karate",
+    "Paddle",
+    "Volley",
+    "Tennis",
+    "Lifting"
+  ];
 
   studentList:any[] = [];
 
   studentName:string = "";
-  studentGender:string = "";
+  studentGender:string = "M";
+  studentHobby:string = "";
+
+  constructor() {
+    for (let index = 0; index < 4; index++) {
+      let num = Math.floor(Math.random() * this.studentRepository.length);
+      let tempStudent = this.studentRepository[num];
+      this.studentList.push(tempStudent);
+      this.studentRepository.splice(num, 1);
+    }
+  }
 
   onAddStudent() {
     let newStudent = {
-      "name": this.studentName, "hobby": "Karate", "gender": this.studentGender, "isPro": false
+      "name": this.studentName, "hobby": this.studentHobby, "gender": this.studentGender, "isPro": false
     }
     this.studentList.push(newStudent);
     this.studentName = "";
